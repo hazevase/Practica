fb1cacfcfc39f771c08e7503c7d6a3cdea2c1b99
Hazevase <TROSENINVLAD@GMAIL.COM>
2024-06-11 17:40:39 +0300

    addhover

--- ff/src/main.jsx
+++ ff/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Components/header/Header.jsx'
-// import Reg from './Components/registration/reg.jsx'
+import Reg from './Components/registration/reg.jsx'
 import Footer from './Components/footer/footer.jsx'
 import Glav from './Components/mainpage/glav.jsx'
 import MyM from './Components/mymusic/mymusic.jsx'
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-      <MyM />
+      <Reg />
       <Footer />
     </BrowserRouter>
   </React.StrictMode>,
